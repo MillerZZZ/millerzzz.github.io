3f7934a5c7f687e8cba4426d6fe71bc7f507898b
Miller Zhang <yufeng.official@gmail.com>
2025-05-14 15:29:05 +0800

    Refactor: use random-utils to replace the random     functions & unify 2 similar scripts

--- pwd/js/main.js
+++ pwd/js/main.js
@@ -1,33 +1,8 @@
 'use strict';
 
-let rand = (() => {
-    const buffer = new Uint32Array(1);
-    const targetRange = 100000;
-    const rangeSize = 0x100000000;
-    const maxValidValue = Math.floor(rangeSize / targetRange) * targetRange;
-
-    return () => {
-      let randomVal;
-      do {
-        window.crypto.getRandomValues(buffer);
-        randomVal = buffer[0];
-      } while (randomVal >= maxValidValue);
-      return randomVal % targetRange;
-    };
-})();
-
-if (!window.crypto || !window.crypto.getRandomValues) {
-    console.warn('Crypto API not supported, using Math.random() instead.');
-    rand = () => Math.floor(Math.random() * 100000);
-}
-
-const randLetter = () => String.fromCharCode('a'.charCodeAt(0) + rand() % 26);
-
-const randNumber = () => String.fromCharCode('0'.charCodeAt(0) + rand() % 10);
-
 const randPwd = () => {
     let pwd = '-****-****-****'.split('');
-    pwd[rand() % 11 > 3 ? rand() % 2 * 5 + 6 + rand() % 4 : rand() % 3 + 2] = randNumber();
+    pwd[RandomUtils.randomBase() % 11 > 3 ? RandomUtils.randomBase() % 2 * 5 + 6 + RandomUtils.randomBase() % 4 : RandomUtils.randomBase() % 3 + 2] = RandomUtils.randomDigit();
     let cnt = 0;
     pwd.forEach((char, index) => {
         if (char == '-')
@@ -39,7 +14,7 @@ const randPwd = () => {
                 let flag = true;
                 do {
                     flag = false;
-                    c = randLetter();
+                    c = RandomUtils.randomLetter();
                     let j = 0;
                     while (++j < cnt && !(flag = pwd[index - j] == c));
                 } while (flag);
@@ -50,7 +25,16 @@ const randPwd = () => {
     return pwd.join('').slice(1);
 };
 
-document.addEventListener('DOMContentLoaded', () => {
-    const genedPwd = randPwd();
+const genedPwd = randPwd();
+if (location.href.endsWith('/pwd/'))
     document.getElementById("rand-pwd").textContent = genedPwd;
-});
+else if (location.href.endsWith('/pwd/json/')) {
+    const responseObj = {
+        code: 200,
+        msg: 'success',
+        data: {
+            pwd: genedPwd
+        }
+    };
+    document.getElementById("rand-pwd").textContent = JSON.stringify(responseObj, null, 2);
+}
